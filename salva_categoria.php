1e704bcdb83157c5138078cff9d7b98066004652
GUSTAVO HENRIQUE DE MOURA <gustavo.hmoura@senacsp.edu.br>
2019-07-10 21:36:36 -0300

    Formatacao da pagina categoria

--- salva_categoria.php
+++ salva_categoria.php
@@ -8,9 +8,9 @@ $descricao = $_POST["descricao"];
     $conexao= mysqli_connect('localhost','root','','ecommerce');
 
       if(mysqli_query($conexao, $query)){
-        echo "<p class='alert-success'> O produto $categoria, foi descrito como: $descricao. </p>";
+        echo "<p class= 'alert-success'> O produto $categoria, foi descrito como: $descricao. </p>";
       } else {
-        echo "<p class= alert-danger'> Tanto a $descricao, quanto sua respectiva categoria $categoria tiveram problemas no cadastramento. TENTE NOVAMENTE MAIS TARDE!";
+        echo "<p class= 'alert-danger'> Tanto a $descricao, quanto sua respectiva categoria $categoria tiveram problemas no cadastramento. TENTE NOVAMENTE MAIS TARDE!";
       }
   include("rodape.php");
 

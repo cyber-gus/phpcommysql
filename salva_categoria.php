ec7e3e36543f62f05a7f5cbd87b3f2ec5ea532f6
GUSTAVO HENRIQUE DE MOURA <gustavo.hmoura@senacsp.edu.br>
2019-07-15 20:24:09 -0300

    Incluiu funcao "funcao"

--- salva_categoria.php
+++ salva_categoria.php
@@ -1,13 +1,19 @@
 <?php
-  include("cabecalho.php");
+      include("cabecalho.php");?>
+
+<?php
+      include("bd.php"); ?>
+<?php
+    function insereCategoria($conexao, $categoria, $descricao){
+      $query= "insert into CATEGORIA (NOME, DESCRICAO) values('{$categoria}','{$descricao}')";
+      $resultadoDoInsere = mysqli_query($conexao, $query);
+      return $resultadoDoInsere;
+    }
+
 $categoria = $_POST["categoria"];
 $descricao = $_POST["descricao"];
 
-  $query= "insert into CATEGORIA (CATEGORIA, DESCRICAO) values({$categoria},{$descricao}) ";
-
-    $conexao= mysqli_connect('localhost','root','','ecommerce');
-
-      if(mysqli_query($conexao, $query)){
+      if(inserecategoria($conexao, $categoria, $descricao)){
         echo "<p class= 'alert-success'> O produto $categoria, foi descrito como: $descricao. </p>";
       } else {
         echo "<p class= 'alert-danger'> Tanto a $descricao, quanto sua respectiva categoria $categoria tiveram problemas no cadastramento. TENTE NOVAMENTE MAIS TARDE!";
--- salva_produto.php
+++ salva_produto.php
@@ -1,12 +1,16 @@
 <?php
   include("cabecalho.php");
-  $nome = $_POST["nome"];
-  $preco = $_POST["preco"];
+  include("bd.php");
+    function insereProduto($conexao, $nome, $preco){
+        $query = "insert into PRODUTO (NOME, PRECO) values ('{$nome}',{$preco})";
+        $resultadoDoInsere = mysqli_query($conexao, $query);
+      return $resultadoDoInsere;
+    }
 
-  $query = "insert into PRODUTO (NOME, PRECO) values ('{$nome}',{$preco})";
+  $nome= $_POST["nome"];
+  $preco= $_POST["preco"];
 
-    $conexao = mysqli_connect('localhost', 'root', '', 'ecommerce');
-  if(mysqli_query($conexao, $query)){
+  if(insereProduto($conexao, $nome, $preco)){
         echo "<p class= 'alert-success'> O produto $nome, no valor $preco reais, foi cadastrado com sucesso. </p>";
       } else {
         echo "<p class= 'alert-danger' > Deu um ruim aqui e o produto não cadastrou.";

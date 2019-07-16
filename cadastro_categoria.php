2c42927127b49506dde3a985103281de007a78f7
GUSTAVO HENRIQUE DE MOURA <gustavo.hmoura@senacsp.edu.br>
2019-07-15 21:38:36 -0300

    Correção cadastro envio

--- cadastro_categoria.php
+++ cadastro_categoria.php
@@ -1,7 +1,7 @@
 <?php include("cabecalho.php"); ?>
       <form action = "/salva_categoria.php" method="post" />
       categoria: <input type ="text" name= "categoria" /> <br />
-      Descriçao: <input type = "number" name= "descricao" /> <br />
+      Descriçao: <input type = "text" name= "descricao" /> <br />
             <input type = "submit" value="Enviar" /> <br>
         </form>
 <?php include("rodape.php"); ?>

bba32f1071b3650b6c1e6ce6238596dafb74d794
GUSTAVO HENRIQUE DE MOURA <gustavo.hmoura@senacsp.edu.br>
2019-07-19 22:13:12 -0300

    Corrigido

--- cadastro_produto.php
+++ cadastro_produto.php
@@ -19,22 +19,29 @@
     echo"<p class= text-danger>Ocorreu um problema. TENTE MAIS TARDE.</p>";
   }
   }
-  ?>
-    <form action= "/salva_produto.php" method= "post">
 
+  if (isset($_GET['id'])){
+    $id = $_GET['id'];
+    $produtoAlteracao = buscarProduto($conexao, $id);
+  ?>
+
+    <form action= "/salva_produto.php" method= "post">
           <table class="table">
+            <input type="hidden" value="<?= $produtoAlteracao['ID'] ?>" name="id"/>
             <tr>
       <td>Nome:</td>
-      <td><input type="text" name="nome" /></td>
+      <td><input type="text" name="nome" value="<?= $produtoAlteracao['NOME'] ?>" /></td>
           </tr>
           <tr>
       <td>Preço:</td>
-      <td><input type="number" name="preco" /></td>
+      <td><input type="number" name="preco" value="<?= $produtoAlteracao['PRECO'] ?>"/></td>
           </tr>
   <tr>
-              <td><input type="submit" class="btn btn-primary" value="cadastrar"/></td>
+              <td><input type="submit" class="btn btn-primary" value="Alterar"/></td>
                 </tr>
 </table>
 </form>
+}
+
 <?php include("listar_produto.php");?>
 <?php include("rodape.php"); ?>
